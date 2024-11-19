6e688b149d5e1a8e4fbde8194d0a8d9761819341
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-11-19 16:07:02 -0300

    agensa wieit

--- agendavisita/wsgi.py
+++ agendavisita/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agedanvisita.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'agendavisita.settings')
 
 application = get_wsgi_application()
