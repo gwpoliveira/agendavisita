9800ab53d6ce7987d7692031c89bd12a2a6ad285
Gleiser Wesley <38473705+gwpoliveira@users.noreply.github.com>
2024-11-22 12:50:08 -0300

    Debug False

--- agendavisita/settings.py
+++ agendavisita/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n83h(#wek5h7zd&b%dtqp4!ddc=1kln)6gf6c%-3yy*(#04s5_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '216.39.249.92','agendavisita.grupomagister.online']
 
